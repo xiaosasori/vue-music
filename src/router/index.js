951c777780354c657a55e41b0c5ab73dc5e753ef
xiaosasori <xiaosasori@gmail.com>
2021-05-17 18:19:06 +0900

    add route guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import store from '@/store';
 import Home from '../views/Home.vue';
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home,
   },
   {
+    name: 'manage',
+    // alias: '/manage',
+    path: '/manage-music',
+    meta: {
+      requiresAuth: true,
+    },
+    component: () => import('../views/Manage.vue'),
+    beforeEnter: (to, from, next) => {
+      console.log('Manage Route Guard');
+      next();
+    },
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+  },
+  {
     path: '/about',
-    name: 'About',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: { name: 'home' },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
+});
+
+router.beforeEach((to, from, next) => {
+  // console.log(to.matched);
+
+  if (!to.matched.some((record) => record.meta.requiresAuth)) {
+    next();
+    return;
+  }
+
+  if (store.state.userLoggedIn) {
+    next();
+  } else {
+    next({ name: 'home' });
+  }
 });
 
 export default router;
